fd68009566772939114c1dc38d88bb8895dc78f1
ohager <oliver@devbutze.com>
2026-05-16 00:21:36 -0300

    ci: Updated docs

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig, coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["src/**/*.test.ts"],
     coverage: {
       reporter: ["text", "json", "html"],
       exclude: [...coverageConfigDefaults.exclude, "docs/**"],
